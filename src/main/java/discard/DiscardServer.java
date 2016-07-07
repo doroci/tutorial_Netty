8b8b069b909982092d0df5fa64d897ff026f23cd
doroci <hiiln0273@gmail.com>
2016-07-08 01:08:53 +0900

    수정

---
+++ src/main/java/discard/DiscardServer.java
@@ -0,0 +1,11 @@
+package discard;
+
+/**
+ * Created by lee on 2016. 7. 8..
+ */
+public class DiscardServer {
+
+    public static void main(String[] args){
+        System.out.println("Hellow world ~");
+    }
+}
